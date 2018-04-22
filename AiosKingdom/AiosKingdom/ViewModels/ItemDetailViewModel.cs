5aba9b9cd1eb236d2ab7652083611d305d287eeb
pampelemoose <zunkwo@gmail.com>
2018-04-22 22:28:48 +0900

    Add project files.

---
+++ AiosKingdom/AiosKingdom/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AiosKingdom.Models;
+
+namespace AiosKingdom.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
