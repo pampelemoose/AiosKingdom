651c21b8d63bf8d6f3024cc78f44fb2c9d7367d0
PampeLeMoose <zunkwo@gmail.com>
2018-12-20 15:55:27 +0900

    Crash reporting
    Closes #27

---
+++ AiosKingdom.Unity/Assets/Scripts/Crasher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Cloud.UserReporting.Plugin;
+using UnityEngine;
+
+public class Crasher : MonoBehaviour
+{
+    public void Start()
+    {
+        if (UnityUserReporting.CurrentClient == null)
+        {
+            UnityUserReporting.Configure();
+        }
+    }
+}
